8bcbc79f8bb7a6a37099d8a0f5908af9e55affca
Marco van Zwetselaar <io@zwets.it>
2021-05-17 02:58:05 +0300

    Release 1.2.0
    - uf errors out when first thing isn't a defline
    - uf transparently consumes gzipped FASTA

--- tests/all-tests.sh
+++ tests/all-tests.sh
@@ -15,44 +15,72 @@
 #
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-#  Created on 2016-01-16
 
 # Function to exit this script with an error message on stderr
-err_exit() {
-	echo "$(basename "$0"): $*" >&2
-	exit 1
-}
+err_exit() { echo "$(basename "$0"): $*" >&2; exit 1; }
 
 # Function to show usage information and exit
 usage_exit() {
-	echo
-	echo "Usage: $(basename $0) [OPTIONS]"
-	echo
-	echo "  Run all tests."
-	echo
-	exit ${1:-1}
+    echo "
+Usage: $(basename $0) [OPTIONS]
+
+  Run all tests.
+
+  OPTIONS
+   -h, --help  Show this information and exit
+" && exit ${1:-1}
 }
 
 # Parse options
 
 while [ $# -ne 0 -a "$(expr "$1" : '\(.\).*')" = "-" ]; do
-	case $1 in
-	-h|--help)
-		usage_exit 0
-		;;
-	*) usage_exit
-		;;
-	esac
-	shift
+    case $1 in
+    -h|--help) usage_exit 0 ;;
+    *) usage_exit ;;
+    esac
+    shift
 done
 
-# @TODO@: look at https://github.com/illusori/bash-tap.git for testing.
+### Test uf
 
-err_exit "
+cd "$(dirname "$0")"
 
-  sole hand in spring forest
-  claps as tree falls soundlessly
-  no tests fail to fail
-"
+E='>Sequence 0
+>Sequence 1
+1234
+>Sequence 2
+1234
+>Sequence 3
+1234
+>Sequence 4
+1234'
+EE="$E
+$E"
 
+UF="../uf"
+T="testdata.fa"
+Z="testdata.fa.gz"
+F="faulty.fa"
+
+# Test UF with valid input
+[ "$E" = "$($UF $T)" ]             || err_exit "failed: test 1"
+[ "$E" = "$(cat $T | $UF)" ]       || err_exit "failed: test 2"
+[ "$E" = "$(cat $T | $UF -)" ]     || err_exit "failed: test 3"
+[ "$EE" = "$(cat $T $T | $UF)" ]   || err_exit "failed: test 4"
+[ "$EE" = "$($UF $T $T)" ]         || err_exit "failed: test 5"
+[ "$EE" = "$(cat $T | $UF $T -)" ] || err_exit "failed: test 6"
+[ "$EE" = "$(cat $T | $UF - $T)" ] || err_exit "failed: test 7"
+
+# Test UF with gzipped valid input
+[ "$E" = "$($UF $Z)" ]             || err_exit "failed: test 1"
+[ "$E" = "$(cat $Z | $UF)" ]       || err_exit "failed: test 2"
+[ "$E" = "$(cat $Z | $UF -)" ]     || err_exit "failed: test 3"
+[ "$EE" = "$(cat $Z $Z | $UF)" ]   || err_exit "failed: test 4"
+[ "$EE" = "$($UF $Z $Z)" ]         || err_exit "failed: test 5"
+[ "$EE" = "$(cat $Z | $UF $Z -)" ] || err_exit "failed: test 6"
+[ "$EE" = "$(cat $Z | $UF - $Z)" ] || err_exit "failed: test 7"
+
+# Test UF with faulty input
+! $UF $F 2>/dev/null || err_exit "failed: test 8" 
+
+# vim: sts=4:sw=4:et:si:ai
